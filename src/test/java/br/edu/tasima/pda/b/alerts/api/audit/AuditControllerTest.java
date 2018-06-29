bbc7caac3430269134c3ebd4650daac339fb6f5c
Tiago Xavier da Silva <xs.tiago@hotmail.com>
2018-06-28 23:11:56 -0300

    Testes notify business

---
+++ src/test/java/br/edu/tasima/pda/b/alerts/api/audit/AuditControllerTest.java
@@ -0,0 +1,12 @@
+package br.edu.tasima.pda.b.alerts.api.audit;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class AuditControllerTest {
+}
--- src/test/java/br/edu/tasima/pda/b/alerts/api/notify/business/AlertBOTest.java
+++ src/test/java/br/edu/tasima/pda/b/alerts/api/notify/business/AlertBOTest.java
@@ -1,6 +1,7 @@
 package br.edu.tasima.pda.b.alerts.api.notify.business;
 
 import br.edu.tasima.pda.b.alerts.api.audit.AuditBO;
+import br.edu.tasima.pda.b.alerts.api.audit.AuditRepository;
 import br.edu.tasima.pda.b.alerts.api.audit.enums.NotifyStatus;
 import br.edu.tasima.pda.b.alerts.api.notify.dto.MetricDTO;
 import br.edu.tasima.pda.b.alerts.api.notify.model.Agenda;
@@ -12,6 +13,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -21,7 +24,7 @@ import java.util.ArrayList;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -32,6 +35,8 @@ public class AlertBOTest {
 
     @MockBean
     private TeamBO teamBO;
+    @MockBean
+    private JavaMailSender javaMailSender;
     @MockBean
     private AuditBO auditBO;
 
@@ -60,10 +65,46 @@ public class AlertBOTest {
 
         Team team = new Team(1L, metricCode, "Suport", engineerList);
 
+        engineer.setTeam(team);
 
         given(teamBO.getByMetricCode(any())).willReturn(team);
-        doNothing().when(auditBO).save(any(Engineer.class), any(MetricDTO.class), NotifyStatus.SUCCESS, any(String.class));
 
         alertBO.sendAlertFromMonitor(metricDTO);
+
+        verify(auditBO).save(engineer, metricDTO, NotifyStatus.SUCCESS, "Email sent.");
+        verify(javaMailSender).send(any(SimpleMailMessage.class));
+    }
+
+    @Test
+    public void sendAlertFromMonitorNoEngineersAvaiableTest() throws Exception {
+        MetricDTO metricDTO = new MetricDTO();
+        metricDTO.setMetric("memory");
+        metricDTO.setThreshold(90);
+        metricDTO.setTimestamp("2018-06-27T03:47:55.038Z");
+        metricDTO.setOrigin("ip-origin");
+        metricDTO.setRule("memory-full");
+        metricDTO.setValue(90);
+
+        MetricCode metricCode = new MetricCode("memory", "memory-full");
+
+        Agenda agenda = new Agenda(1L, null, LocalDateTime.now().getDayOfWeek(), LocalTime.now(), LocalTime.now());
+
+        ArrayList<Agenda> agendaList = new ArrayList<>();
+        agendaList.add(agenda);
+
+        Engineer engineer = new Engineer(1L, null, "John Johnes", "jj@gmail.com", agendaList);
+
+        ArrayList<Engineer> engineerList = new ArrayList<>();
+        engineerList.add(engineer);
+
+        Team team = new Team(1L, metricCode, "Suport", engineerList);
+
+        engineer.setTeam(team);
+
+        given(teamBO.getByMetricCode(any())).willReturn(team);
+
+        alertBO.sendAlertFromMonitor(metricDTO);
+
+        verify(auditBO).save(null, metricDTO, NotifyStatus.FAILURE, "No Engineers available at this moment!.");
     }
 }
